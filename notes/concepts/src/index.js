806d3648957c96086656a9910b758147d6dd555c
praveen <itasworkshop@gmail.com>
2021-06-10 12:15:54 +0530

    10 June

--- notes/concepts/src/index.js
+++ notes/concepts/src/index.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/* class App extends React.Component{
-  render(){
-    return(
-      <div className="App">
-        <h1>Hello World!!!</h1>
+class Clock extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount(){
+    this.timerId = setInterval(
+      () => this.tick(),1000
+    );
+  }
+
+  tick() {
+    //this.state = new Date();
+    this.setState({ date: new Date() });
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello There</h1>
+        <h2> It is {this.state.date.toLocaleTimeString()}</h2>
       </div>
     );
   }
-} */
 
-function App(props){
-  return <h1>Hello World!!!, {props.name} </h1>
 }
 
-ReactDOM.render(<App name="Rajesh"/>,document.getElementById("root") );
+ReactDOM.render(
+  <Clock />
+  ,document.getElementById("root") );
