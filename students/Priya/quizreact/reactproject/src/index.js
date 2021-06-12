8be05bda3a4477a47f0fc78cbe2f57560efdb55f
priyasingh15 <singhpriyaa123@gmail.com>
2021-06-13 01:13:39 +0530

    12 June 4

--- students/Priya/quizreact/reactproject/src/index.js
+++ students/Priya/quizreact/reactproject/src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import imageOne from  "./image/OFFbulb.jpg";
+import imageTwo from  "./image/ONbulb.jpg";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class ToggleBothImage extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {isOnToggle:true}
+    this.myHandler = this.myHandler.bind(this);
+  }
+  myHandler(){
+    this.setState( state => ({
+      isOnToggle : !state.isOnToggle,
+    })
+
+    );
+
+    console.log("toggle Images");
+  }
+
+render(){
+ 
+  return(
+    <div onClick={this.myHandler}>
+      {this.state.isOnToggle ?
+      <img src={imageOne} alt=""/> :
+      <img src={imageTwo} alt=""/>
+
+      }
+    
+    </div>
+  )
+}
+
+
+
+}
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <ToggleBothImage />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
