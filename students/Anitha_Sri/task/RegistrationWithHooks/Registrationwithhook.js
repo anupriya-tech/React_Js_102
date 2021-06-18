f28d6e4c90004320a7c116d582985ece1e8509a7
AnithaSatish <70425761+AnithaSatish@users.noreply.github.com>
2021-06-18 10:51:06 +0530

    Committed 18.06.2021

---
+++ students/Anitha_Sri/task/RegistrationWithHooks/Registrationwithhook.js
@@ -0,0 +1,30 @@
+import React,{useState} from 'react';
+
+
+function LoginWithHook(){
+    const[user, setUser] = useState({username:'', password:''})
+  
+    
+    const handleLogin=(login)=>{ login.preventDefault();
+      alert("Successfully loggedIn")
+     }
+      const handlelogout=(logout)=>{logout.preventDefault();
+      alert("Please login")
+      }
+         return(
+           <div>
+             <p><b>Login Page</b></p>
+           <label> UserName : 
+             <input type="text" value={user.username.value} ></input>
+           </label><br></br>
+  
+            <label> Password   : 
+            <input type="text" value={user.password.value} ></input>
+          </label><br></br>
+            <button onClick={handleLogin}>Login</button>
+             <button onClick={handlelogout}>Logout</button>
+        </div>
+         )
+       
+    }
+    export default LoginWithHook
